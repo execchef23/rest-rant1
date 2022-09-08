844d5d21418e73e63b3762d8fe907b2b39c1ad39
Michael Manley <michaelmanley@Michaels-MacBook-Pro.local>
2022-09-08 14:47:17 -0400

    Updated act 8

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -2,32 +2,31 @@ const React = require('react')
 const Def = require('../default')
 
 function index (data) {
-  let placesFormatted = data.places.map((place, index) => {
-    return (
-      <div className="col-sm-6">
-        <h2>
-          <a href={`/places/${index}`} >
-            {place.name}
-          </a>
-        </h2>
-        <p className="text-center">
-          {place.cuisines}
-        </p>
-        <img src={place.pic} alt={place.name} />
-        <p className="text-center">
-          Located in {place.city}, {place.state}
-        </p>
-      </div>
-    )
-  })
-  
-    return (
-      <Def>
-          <main>
-              <h1>PLACES INDEX PAGE</h1>
+  let placesFormatted = data.places.map((place) => (
+    <div className="col-sm-6">
+      <h2>
+        <a href={`/places/${place.id}`}>
+          {place.name}
+        </a>
+      </h2>
+      <p className="text-center">
+        {place.cuisines}
+      </p>
+      <img src={place.pic} alt={place.name} />
+      <p className="text-center">
+        Located in {place.city}, {place.state}
+      </p>
+    </div>
+  ))
+  return (
+    <Def>
+        <main>
+            <h1>Places to Rant or Rave About</h1>
+            <div className="row">
               {placesFormatted}
-          </main>
-      </Def>
+            </div>
+        </main>
+    </Def>
   )
 }
 
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,29 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
+    let comments = (
+        <h3 className="inactive">
+          No comments yet!
+        </h3>
+      )
+      let rating = (
+        <h3 className="inactive">
+          Not yet rated
+        </h3>
+      )
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <h1>{ data.place.name }</h1>
+            <a href={`/places/${data.place.id}/edit`} className="btn btn-warning"> 
+                Edit
+            </a>  
+            <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}> 
+                <button type="submit" className="btn btn-danger">
+                    Delete
+                </button>
+            </form>     
           </main>
         </Def>
     )
